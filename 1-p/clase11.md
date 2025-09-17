00f7c5402136ee6dbf168c2b452590f58114b1e6
p3p3p3k4z <mario.ness64@gmail.com>
2025-09-17 12:47:00 -0600

    feat: triangulos&repaso

---
+++ 1-p/clase11.md
@@ -0,0 +1,18 @@
+# Repaso para examen
+
+
+funcion de alto orden, toma un operador yt dos listas de ejemplo. este tiene tres argumentos
+zipWith
+
+como sacar la cola de una lista
+tail [1,2.3..]
+sacar la cabeza de una lista
+head [1,2,3..]
+
+perfilador
+set +s 
+
+que hace where
+nombramiento diferido 
+
+contruir en forma de let anticipado
---
+++ 1-p/clase11/triangulo.hs
@@ -0,0 +1,21 @@
+{- triangulo de pascal -}
+{- triangulo de slerpinsky -}
+trian 0 = [1]
+trian n|n>0 = [1]++(zipWith(+)(trian(n-1)) (tail(trian(n-1)))++[1]
+
+trianDiferido {} = {1}
+trianDiferido n | n>0 = 
+    {1}++(zipWith(+)ls (tail ls))++[1]
+    where 
+      ls=trianDiferido(n-1)
+
+
+trianAnticipado {} = {1}
+trianAnticipado n | n>0 = let ls = trianAnticipado (n-1) in 
+              [1]++(zipWith(+)ls (tail ls))++[1]
+imprimeLista[]= ""
+imprimeLista(a:bs)=(show a):imprimeLista bs
+
+-- calcular? anticipado?|formato binario
+-- map (\x -> if odd(x) then 1 else 0)(trianAnticipado 25)
+--- imprimir el triangulo en forma escalonada
