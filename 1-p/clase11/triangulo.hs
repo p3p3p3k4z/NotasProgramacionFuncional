c0ecfc98632bcb450dcadb8d4125197f10d65cba
p3p3p3k4z <mario.ness64@gmail.com>
2025-09-20 12:53:46 -0600

    feat:func&list

--- 1-p/clase11/triangulo.hs
+++ 1-p/clase11/triangulo.hs
@@ -13,9 +13,16 @@ trianDiferido n | n>0 =
 trianAnticipado {} = {1}
 trianAnticipado n | n>0 = let ls = trianAnticipado (n-1) in 
               [1]++(zipWith(+)ls (tail ls))++[1]
-imprimeLista[]= ""
+
+tabulador n | n>=0 = concat(take n (repaeat ".\t"))
+
+imprimeLista[]= "listo"
 imprimeLista(a:bs)=(show a):imprimeLista bs
 
 -- calcular? anticipado?|formato binario
 -- map (\x -> if odd(x) then 1 else 0)(trianAnticipado 25)
 --- imprimir el triangulo en forma escalonada
+
+main=do
+--	read n :: Integer % <- getLine
+	mapM_putStrLn (map show(map trian[0..20]))--]))
---
+++ 1-p/clase12.md
@@ -0,0 +1,16 @@
+# clase 12
+
+- pregunta de examen
+  como compilar un programa:  
+
+- filtrar elementos
+  filter (<10) [23,23,4,5,3,2]
+  filter (old)
+  filter (even)
+
+  filter (\x -> (snd x)<12)[("PORCHE", 23),("BMW",4),(VOCHO,1)]
+  let f(x,y,z) = z in f ("bmw",234,"blanco")
+  filter (\x -> ((ord x) && (x<10) || (x>100))) [1,3,2,5,6,3,33,3]
+- foldr
+  foldr (+) [1..100]
+  foldr (++) ["hola"."mundo",":D"]
