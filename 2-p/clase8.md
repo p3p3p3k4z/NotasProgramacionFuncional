008d5cc8e339b6c38315ebc2f065e79f3728e9f5
p3p3p3k4z <mario.ness64@gmail.com>
2025-10-14 12:50:41 -0600

    feat:camel

---
+++ 2-p/clase8.md
@@ -0,0 +1,14 @@
+### Un paseo por Camel
+
+```pl
+# let f(x)=x*x;
+val f : int -> int = <fun>
+#f(2)+f(4);;
+- : int = 20
+
+let g(x) = x+1;;
+val g : int -> int = <fun>
+#g/f(10)+1;;
+```
+
+
---
+++ 2-p/clase8/propioOrita.pl
@@ -0,0 +1,26 @@
+% A -- > B --> C 
+% A --> A 
+
+rw(A,C):-t(A,B),rw(B,C).
+% rw(A,C):-A=>B,rw(B,C)
+rw(A,A)
+
+% cambiar pero no puedo
+
+% posibilidad de cambiar 
+
+r(A+B,C):-number(A),number(C)
+  C is A+B
+r(A+B,A1+B):-r(A,A1)
+% (A+B) => C :- number(A), number(B)
+%     C is A+B
+r(A+B,A+B1):-r(B,B1)
+r(A*B,A1*B):-r(A,A1)
+r(A*B,A*B1):-r(B,B1)
+
+fib(0)=>1:-true.
+fib(1)=>1:-true.
+% r(fib(0),1)-
+r(fib(N),fib(N1)+fib(N2)):-
+N1 is N-1,
+N2 is N-2.
